c8cc4573dabf67e097198c74c0e937875c41b9be
Chris Ellis <ellis.cs@gmail.com>
2020-09-02 07:04:45 -0400

    Control back button

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,14 +1,35 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
-const Header = () => (
-  <header className="">
-    <h1 className="">
-      <Link to="/" className="text-orange-500">
-        {'<<'} Home
-      </Link>
-    </h1>
-  </header>
-)
+const Header = () => {
+
+  // Get url at run time
+  const url = typeof window !== 'undefined' ? window.location.href : '';
+  // Options to go back on
+  const linkLocations = [
+    {
+      match: "/project/",
+      text: "Projects",
+      route: "/projects"
+    },
+    {
+      match: "/blog/",
+      text: "Blog",
+      route: "/blog"
+    }
+  ]
+  // Match or return home
+  const match = linkLocations.find(linkLocation => url.includes(linkLocation.match)) || { text: "Home", route: "/" }
+
+  return (
+    <header className="">
+      <h1 className="">
+        <Link to={match.route} className="text-orange-500">
+          {'<<'} {match.text}
+        </Link>
+      </h1>
+    </header>
+  )
+}
 
 export default Header
