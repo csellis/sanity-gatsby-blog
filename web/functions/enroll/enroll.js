f1626badd6ae1f8d85927461314fddd23ece174a
Chris Ellis <ellis.cs@gmail.com>
2020-09-15 06:13:12 -0400

    Test adding email

---
+++ web/functions/enroll/enroll.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+
+const fetch = require("node-fetch");
+const base64 = require('base-64')
+
+exports.handler = async function (event, context) {
+  // console.log('Subscribe API Hit')
+  // Only allow POST
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+  const errorGen = msg => ({ statusCode: 500, body: msg })
+  try {
+    // console.log(event.body)
+    const { email } = JSON.parse(event.body)
+    if (!email) {
+      return errorGen('Missing Email')
+    }
+    const subscriber = {
+      email_address: email,
+      status: 'subscribed',
+    }
+    const creds = `any:${process.env.MAILCHIMP_KEY}`
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: "You've signed up to the mailing list!",
+        subscriber,
+        creds,
+        body: event.body
+      }),
+    }
+
+
+    // const response = await fetch(
+    //   'https://us3.api.mailchimp.com/3.0/lists/a265105f9b/members/',
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       Accept: '*/*',
+    //       'Content-Type': 'application/json',
+    //       Authorization: `Basic ${base64.encode(creds)}`,
+    //     },
+    //     body: JSON.stringify(subscriber),
+    //   }
+    // )
+
+    // const data = await response.json()
+    // if (!response.ok) {
+    //   // NOT res.status >= 200 && res.status < 300
+    //   return { statusCode: data.status, body: data.detail }
+    // }
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     msg: "You've signed up to the mailing list!",
+    //     detail: data,
+    //   }),
+    // }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }),
+    }
+  }
+};
