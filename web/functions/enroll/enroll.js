02e53f67a12357905ea52baab0ed090d3aba758e
Chris Ellis <ellis.cs@gmail.com>
2020-09-25 19:17:26 -0400

    Working on tags

--- web/functions/enroll/enroll.js
+++ web/functions/enroll/enroll.js
@@ -3,62 +3,82 @@
 
 const fetch = require("node-fetch");
 const base64 = require('base-64')
+const mailchimp = require('@mailchimp/mailchimp_marketing')
+const md5 = require("md5");
+
 
 exports.handler = async function (event, context) {
-  // console.log('Subscribe API Hit')
+  console.log('Subscribe API Hit')
   // Only allow POST
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
+
   const errorGen = msg => ({ statusCode: 500, body: msg })
+
   try {
-    // console.log(event.body)
-    const { email } = JSON.parse(event.body)
+    const SERVER_PREFIX = "us3"
+    const LIST_ID = "a265105f9b"
+
+    mailchimp.setConfig({
+      apiKey: process.env.MAILCHIMP_KEY,
+      server: SERVER_PREFIX,
+    });
+
+    const { email, tags } = JSON.parse(event.body);
+    console.log({ tags })
+
     if (!email) {
       return errorGen('Missing Email')
     }
-    const subscriber = {
-      email_address: email,
-      status: 'subscribed',
+
+    const subscriberHash = md5(email.toLowerCase());
+
+    const response = await mailchimp.lists.setListMember(
+      LIST_ID,
+      subscriberHash,
+      {
+        email_address: email,
+        status_if_new: "pending",
+        status: "pending"
+      }
+    );
+
+    // add Tags
+    if (tags) {
+      // compose with html
+      const putUrl = `https://server.api.mailchimp.com/3.0/lists/${LIST_ID}/members/${subscriberHash}/tags`
+
+      const body = {
+        tags: tags
+      };
+
+      fetch(putUrl, {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+      })
+        .then(res => {
+          console.log({ response: res.json() })
+        })
+        .then(json => console.log(json));
+      // const tagsResponse = await mailchimp.lists.updateListMemberTags(
+      //   LIST_ID,
+      //   subscriberHash,
+      //   tags
+      // );
+      // console.log({ tagsResponse })
     }
-    const creds = `any:${process.env.MAILCHIMP_KEY}`
+
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        msg: "You've signed up to the mailing list!",
-        subscriber,
-        creds,
-        body: event.body
-      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(response)
     }
 
-
-    // const response = await fetch(
-    //   'https://us3.api.mailchimp.com/3.0/lists/a265105f9b/members/',
-    //   {
-    //     method: 'POST',
-    //     headers: {
-    //       Accept: '*/*',
-    //       'Content-Type': 'application/json',
-    //       Authorization: `Basic ${base64.encode(creds)}`,
-    //     },
-    //     body: JSON.stringify(subscriber),
-    //   }
-    // )
-
-    // const data = await response.json()
-    // if (!response.ok) {
-    //   // NOT res.status >= 200 && res.status < 300
-    //   return { statusCode: data.status, body: data.detail }
-    // }
-    // return {
-    //   statusCode: 200,
-    //   body: JSON.stringify({
-    //     msg: "You've signed up to the mailing list!",
-    //     detail: data,
-    //   }),
-    // }
   } catch (err) {
     console.log(err) // output to netlify function log
     return {
